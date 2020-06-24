f86ea63338a214213d071da7f33d3b232fd47ec4
OISHIChowdhury2 <66108712+OISHIChowdhury2@users.noreply.github.com>
2020-06-24 21:53:41 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+var arr=["jone","Jake"]
+var leght=arr.length;
+var string=arr
+ var arr1=["rishat","oishi"]
+ var leght1=arr1.length;
+for (var i = 0; i < leght; i++) {
+    console.log("Hello"+arr[i]);
+   
+    //Do something
+}
+for(var j=0;j<leght1;j++)
+{
+    console.log("goodbye"+arr1[j]);
+}
